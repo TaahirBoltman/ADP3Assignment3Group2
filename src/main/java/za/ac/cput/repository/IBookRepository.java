2fb4d97d35963edb7c391ac22945225456f4c752
TaahirBoltman <62078864+TaahirBoltman@users.noreply.github.com>
2021-10-19 21:12:42 +0200

    Delete IBookRepository.java

--- src/main/java/za/ac/cput/repository/IBookRepository.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cput.repository;
-
-import za.ac.cput.entity.Book;
-
-import java.util.*;
-
-public interface IBookRepository extends IRepository<Book, String>{
-    public Set<Book> getAll();
-}
